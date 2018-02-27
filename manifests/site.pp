8eb30d83b25e727a29fac35ac56befbd9b20a1cd
madhu1608 <34922739+madhu1608@users.noreply.github.com>
2018-02-28 02:48:23 +0530

    used require1

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,11 @@
-node default {    # applies to ns1 and ns2 nodes
+node default { 
 
-  include '::archive' # NOTE: optional for posix platforms
+  include '::archive'
   $prereq_packages = ['telnet', 'wget', 'unzip']
   $prereq_apache_folders = ['/opt', '/opt/SP']
-  $install_apache_folder = '/opt/SP/apache'
+  $install_apache_folder = ['/opt/SP/apache', '/opt/SP/apache/httpd-2.4.29']
   $docs_apache_folder = '/opt/SP/apache/httpd-2.4.29/htdocs'
+  $httpd_service_abspath = '/etc/systemd/system/httpd.service'
 
   package { $prereq_packages:
     ensure => "present",
@@ -16,27 +17,29 @@ node default {    # applies to ns1 and ns2 nodes
 
   file { $install_apache_folder:
     ensure => 'directory',
+    require => File[$prereq_apache_folders],
   }
 
   archive { '/tmp/phpenabled_httpd-2.4.29.tar.gz':
     ensure        => present,
     extract       => true,
     extract_path  => $install_apache_folder,
-    source        => 'http://192.168.1.2:8081/nexus/content/repositories/proj01/httpd/2.4.29/phpenabled_httpd-2.4.29.tar.gz',
-    #checksum      => '2ca09f0b36ca7d71b762e14ea2ff09d5eac57558',
-    #checksum_type => 'sha1',
-    #creates       => '/tmp/javax',
+    source        => 'http://192.168.43.9:8081/nexus/content/repositories/proj01/httpd/2.4.29/phpenabled_httpd-2.4.29.tar.gz',
     cleanup       => true,
+    require => File[$install_apache_folder],
+  }
+
+  file { $httpd_service_abspath: 
+    ensure => 'file',
+    source => 'puppet:///extra_files/httpd.service',
+    require => Archive['/tmp/phpenabled_httpd-2.4.29.tar.gz'],
   }
 
   archive { '/tmp/php-7.1.12.tar.gz':
     ensure        => present,
     extract       => true,
     extract_path  => $install_apache_folder,
-    source        => 'http://192.168.1.2:8081/nexus/content/repositories/proj01/php/7.1.12/php-7.1.12.tar.gz',
-    #checksum      => '2ca09f0b36ca7d71b762e14ea2ff09d5eac57558',
-    #checksum_type => 'sha1',
-    #creates       => '/tmp/javax',
+    source        => 'http://192.168.43.9:8081/nexus/content/repositories/proj01/php/7.1.12/php-7.1.12.tar.gz',
     cleanup       => true,
   }
 
@@ -44,28 +47,15 @@ node default {    # applies to ns1 and ns2 nodes
     ensure        => present,
     extract       => true,
     extract_path  => $docs_apache_folder,
-    source        => 'http://192.168.1.2:8081/nexus/content/repositories/proj01/pdo/pdo_rel_1_1/pdo.tar.gz',
-    #checksum      => '2ca09f0b36ca7d71b762e14ea2ff09d5eac57558',
-    #checksum_type => 'sha1',
-    #creates       => '/tmp/javax',
-    #cleanup       => true,
-  }
-
-  file { '/etc/systemd/system/httpd.service': 
-    #path => '/root/',
-    #path => '/etc/systemd/system',
-    ensure => 'file',
-    source => 'puppet:///extra_files/httpd.service',
-    #source => 'https://github.com/madhu1608/pdodeps/blob/master/httpd.service',
+    source        => 'http://192.168.43.9:8081/nexus/content/repositories/proj01/pdo/pdo_rel_1_1/pdo.tar.gz',
   }
 
   file { '/opt/SP/apache/httpd-2.4.29/conf/httpd.conf':
-    #path => '/opt/SP/apache/httpd-2.4.29/conf',
     ensure => 'file',
     source => 'puppet:///extra_files/httpd.conf',
     notify  => Service['httpd'],
+    require => Archive['/tmp/php-7.1.12.tar.gz', '/tmp/pdo.tar.gz'], File[$httpd_service_abspath]
   }
-
 
   service { "httpd":
     ensure  => running,
@@ -73,7 +63,7 @@ node default {    # applies to ns1 and ns2 nodes
     start   => "/opt/SP/apache/httpd-2.4.29/bin/apachectl -k start -f /opt/SP/apache/httpd-2.4.29/conf/httpd.conf",
     stop    => "/opt/SP/apache/httpd-2.4.29/bin/apachectl -k stop -f /opt/SP/apache/httpd-2.4.29/conf/httpd.conf",
     restart => "/opt/SP/apache/httpd-2.4.29/bin/apachectl -k graceful -f /opt/SP/apache/httpd-2.4.29/conf/httpd.conf",
-    pattern => "/opt/SP/apache/httpd-2.4.29/bin",
+    pattern => "/opt/SP/apache/httpd-2.4.29/bin/httpd",
   }
 
 }
