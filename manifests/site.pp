9a51d73b86a457383e963fbdc1d12addf4257277
madhu1608 <34922739+madhu1608@users.noreply.github.com>
2018-04-01 00:19:11 +0530

    fixerrr

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,16 @@
-node default { 
+node default {
 
-  include '::archive'
-  $prereq_packages = ['telnet', 'wget', 'unzip']
-  $prereq_apache_folders = ['/opt', '/opt/SP']
-  $install_apache_folder = ['/opt/SP/apache', '/opt/SP/apache/httpd-2.4.29']
-  $docs_apache_folder = '/opt/SP/apache/httpd-2.4.29/htdocs'
-  $httpd_service_abspath = '/etc/systemd/system/httpd.service'
+	include ::archive
 
-  package { $prereq_packages:
-    ensure => "present",
+	$nexus_repo_base = 'http://nexus:8081/repository/locdev-phpproj'
+	$prereq_apache_folders = ['/opt', '/opt/SP']
+  $install_apache_folder = '/opt/SP/apache'
+  $minimal_packages = ['telnet', 'wget', 'bzip2', 'curl']
+
+  package { $minimal_packages :
+    ensure => 'present',
   }
+
 
   file { $prereq_apache_folders:
     ensure => 'directory',
@@ -20,41 +21,30 @@ node default {
     require => File[$prereq_apache_folders],
   }
 
-  archive { '/tmp/phpenabled_httpd-2.4.29.tar.gz':
-    ensure        => present,
-    extract       => true,
-    extract_path  => $install_apache_folder,
-    source        => 'http://192.168.43.9:8081/nexus/content/repositories/proj01/httpd/2.4.29/phpenabled_httpd-2.4.29.tar.gz',
-    cleanup       => true,
-    require => File[$install_apache_folder],
-  }
 
-  file { $httpd_service_abspath: 
-    ensure => 'file',
-    source => 'puppet:///extra_files/httpd.service',
-    require => Archive['/tmp/phpenabled_httpd-2.4.29.tar.gz'],
-  }
+	archive { '/tmp/httpd/phpenabled_httpd-2.4.29.tar.gz':
+		source       => "${nexus_repo_base}/httpd/2.4.29/phpenabled_httpd-2.4.29.tar.gz",
+		extract      => true,
+		extract_path => '/opt/SP/apache',
+		creates      => '/opt/SP/apache/httpd-2.4.29',
+		cleanup      => false,
+		require => File[$install_apache_folder],
+	}
 
-  archive { '/tmp/php-7.1.12.tar.gz':
-    ensure        => present,
-    extract       => true,
-    extract_path  => $install_apache_folder,
-    source        => 'http://192.168.43.9:8081/nexus/content/repositories/proj01/php/7.1.12/php-7.1.12.tar.gz',
-    cleanup       => true,
-  }
-
-  archive { '/tmp/pdo.tar.gz':
-    ensure        => present,
-    extract       => true,
-    extract_path  => $docs_apache_folder,
-    source        => 'http://192.168.43.9:8081/nexus/content/repositories/proj01/pdo/pdo_rel_1_1/pdo.tar.gz',
-  }
+	archive { '/tmp/httpd/pdo_rel_1_1.tar.gz':
+		source       => "${nexus_repo_base}/pdo/rel_1_1/pdo_rel_1_1.tar.gz",
+		extract      => true,
+		extract_path => '/opt/SP/apache/httpd-2.4.29/htdocs',
+		creates      => '/opt/SP/apache/httpd-2.4.29/htdocs/pdo',
+		cleanup      => false,
+		require => Archive['/tmp/httpd/phpenabled_httpd-2.4.29.tar.gz'],
+	}
 
   file { '/opt/SP/apache/httpd-2.4.29/conf/httpd.conf':
     ensure => 'file',
     source => 'puppet:///extra_files/httpd.conf',
     notify  => Service['httpd'],
-    require => Archive['/tmp/php-7.1.12.tar.gz', '/tmp/pdo.tar.gz'], File[$httpd_service_abspath]
+    require => Archive['/tmp/httpd/pdo_rel_1_1.tar.gz'],
   }
 
   service { "httpd":
@@ -62,7 +52,7 @@ node default {
     hasrestart => true,
     start   => "/opt/SP/apache/httpd-2.4.29/bin/apachectl -k start -f /opt/SP/apache/httpd-2.4.29/conf/httpd.conf",
     stop    => "/opt/SP/apache/httpd-2.4.29/bin/apachectl -k stop -f /opt/SP/apache/httpd-2.4.29/conf/httpd.conf",
-    restart => "/opt/SP/apache/httpd-2.4.29/bin/apachectl -k graceful -f /opt/SP/apache/httpd-2.4.29/conf/httpd.conf",
+    restart => "/opt/SP/apache/httpd-2.4.29/bin/apachectl -k restart -f /opt/SP/apache/httpd-2.4.29/conf/httpd.conf",
     pattern => "/opt/SP/apache/httpd-2.4.29/bin/httpd",
   }
 
