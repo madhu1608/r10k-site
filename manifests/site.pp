8bb607d42a736df1304d253f4076c89a3ec4328b
Madhusudhan Mukthavaram <madhu1608@hotmail.com>
2018-04-10 01:58:59 +0200

    xxx

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,14 @@
 node default {
 
+
+}
+
+node 'puppet.locdev.com' {
+	file { '/etc/puppetlabs/puppet/autosign.conf':
+  	ensure => link,
+  	target => '/etc/puppetlabs/code/environments/production/autosign.conf',
+	}
+
 	file { '/etc/puppetlabs/puppet/fileserver.conf':
   	ensure => link,
   	target => '/etc/puppetlabs/code/environments/production/fileserver.conf',
@@ -9,6 +18,5 @@ node default {
     ensure => 'file',
     source => 'puppet:///extra_files/hosts',
   }
+
 }
-
-
