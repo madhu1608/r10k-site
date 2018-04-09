02f6cfeb9671d4e9c26f167ac0df264adcd3b4af
Madhusudhan Mukthavaram <madhu1608@hotmail.com>
2018-04-10 01:48:49 +0200

    xxx

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,12 @@
 node default {
-  file { '/etc/hosts':
-    ensure => 'file',
-    source => 'puppet:///extra_files/hosts',
-  }
+
+	file { '/root/linkfile':
+  	ensure => link,
+  	target => '/root/mainfile',
+	}
+
+  #file { '/etc/hosts':
+  #  ensure => 'file',
+  #  source => 'puppet:///extra_files/hosts',
+  #}
 }
